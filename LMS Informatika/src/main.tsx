22b5a6149b546ac739091bc25ff8e4216d0fc636
iskandarpatue <71415692+iskandarpatue@users.noreply.github.com>
2026-08-02 14:13:28 +0800

    Fix import statement for App component

--- LMS Informatika/src/main.tsx
+++ LMS Informatika/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // atau './lms_informatika_sma_kelas_10' sesuai nama file TSX Anda
+// Memanggil file program LMS Anda yang ada di folder yang sama
+import App from './App' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
