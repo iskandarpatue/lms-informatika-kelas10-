b4b1b54226051b1c5829562a57e298068f2ba38a
iskandarpatue <71415692+iskandarpatue@users.noreply.github.com>
2026-08-02 19:08:32 +0800

    Remove commented line in main.tsx

--- LMS Informatika/src/main.tsx
+++ LMS Informatika/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Memanggil file program LMS Anda yang ada di folder yang sama
-import App from './App' 
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
