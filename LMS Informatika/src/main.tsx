c1ac050b56c49498b5c6d932bec13176a195899a
iskandarpatue <71415692+iskandarpatue@users.noreply.github.com>
2026-08-02 14:03:56 +0800

    Refactor main.tsx to include CSS and StrictMode
    Updated import statement and added React.StrictMode wrapper.

--- LMS Informatika/src/main.tsx
+++ LMS Informatika/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App' // atau './lms_informatika_sma_kelas_10' sesuai nama file TSX Anda
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  
-    
-  ,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
